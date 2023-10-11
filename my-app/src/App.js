7e0bebf0a05d13313c12bcbd5e99014b46af83cb
oyeong011 <oyeong011@naver.com>
2023-10-11 10:26:11 +0900

    10.11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+  let [count, setCount] = useState(0);
+  let [age, setAge] = useState(20);
+  
+  useEffect(()=>{
+    count != 0 && count < 3 ? setAge(age+1) : setAge(age)
+  },[count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>안녕하십니까 전 {age}</div>
+      <button onClick={()=>{
+          setCount(count+1)
+          console.log("누름")
+      }}>누르면한살먹기</button>
     </div>
-  );
+  )
+
 }
 
 export default App;
