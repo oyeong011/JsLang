66d5599d114cf23e374667b619687983ad7e86bb
oyeong011 <oyeong011@gmail.com>
2023-11-13 11:46:31 +0900

    Feat : React 세팅

---
+++ inflearn/react/todo-app/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, {Component} from 'react';
+
+
+export default class App extends Compone {
+  render(){
+    return (
+      <div className="container">
+        안여
+        
+      </div>
+    );
+  }
+}
+
+export default App;
