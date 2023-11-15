e6eab8eaace51371bd1e6a962f282b8c2ffbc714
oyeong011 <oyeong011@gmail.com>
2023-11-15 14:59:51 +0900

    CRUD앱 만들기

---
+++ inflearn/react/todo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-amber-600 w-screen h-screen flex items-center'>
+      <div className='w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg'>
+        <div className='flex justify-between mb-3'>
+          <h1>예산 계산기</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
